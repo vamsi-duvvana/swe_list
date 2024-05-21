2249ff048a51019dd5d55732f83b1f77a2823374
SAISURYAtalla <tallasai@iitbhilai.ac.in>
2024-05-22 00:03:01 +0530

    hamburger menu and header responsiveness added

--- src/components/RightHeader.js
+++ src/components/RightHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RightHeader = ({ githubLink, aboutLink }) => {
 return (
-    <div className="flex items-center justify-center space-x-4 p-4 sm:p-6">
+    <div className="flex items-center justify-center space-x-2 sm:space-x-4 p-2 sm:p-4">
     <a href={aboutLink} target="_blank" rel="noopener noreferrer" className="text-sm sm:text-base lg:text-lg text-black hover:text-gray-700">
         About Us
     </a>
